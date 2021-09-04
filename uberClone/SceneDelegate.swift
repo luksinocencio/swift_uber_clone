1e1d51cbc8a64d531cc2f6127edfdd0a76c63949
devmeist3r <devmeist3r@gmail.com>
2021-09-04 04:20:48 -0300

    *

--- uberClone/SceneDelegate.swift
+++ uberClone/SceneDelegate.swift
@@ -4,13 +4,10 @@ class SceneDelegate: UIResponder, UIWindowSceneDelegate {
     var window: UIWindow?
     
     func scene(_ scene: UIScene, willConnectTo session: UISceneSession, options connectionOptions: UIScene.ConnectionOptions) {
-        
         guard let windowScene = (scene as? UIWindowScene) else { return }
-        window = UIWindow(frame: UIScreen.main.bounds)
-        let viewController = HomeController()
-        window?.rootViewController = viewController
+        window = UIWindow(windowScene:  windowScene)
+        window?.rootViewController = HomeController()
         window?.makeKeyAndVisible()
-        window?.windowScene = windowScene
     }
 }
 
